fceb9f1cbfe02fb3850bdcef2fc415e2bbb0adb3
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-02-25 18:15:09 +0530

    Delete

--- assets/js/loading.js
+++
@@ -1,5 +0,0 @@
- window.addEventListener("load", function() {
-      document.getElementById("loadingani").style.display = "none";
-      document.getElementById("myimg").style.visibility = "";
-
-})
