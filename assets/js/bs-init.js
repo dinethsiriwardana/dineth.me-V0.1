52ecdc316516e486c6655e8c4d7cd0cf0c9c2507
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-02-25 20:33:43 +0530

    Add 1&0s

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -14,6 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		AOS.init();
 	}
 
+	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
+	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
+		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
+		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
+	});
+
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	  return new bootstrap.Tooltip(tooltipTriggerEl);
